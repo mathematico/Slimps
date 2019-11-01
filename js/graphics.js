62f3c474a122a391124e3f6fe210cb55fb34392a
mathematico <47189734+mathematico@users.noreply.github.com>
2019-11-01 12:57:02 -0400

    Update graphics.js

--- js/graphics.js
+++ js/graphics.js
@@ -13,9 +13,9 @@ function drawmap(){
     for(var x=0; x<=tmap[y].length-1;++x){
       console.log(x,y)
       if (tmap[y][x]==1){
-        ctx.drawImage(img_gazon, x*tmpa_tw +tmap_x, y*tmpa_th+tmap_y,tmpa_tw,tmpa_th)
+        ctx.drawImage(img_gazon, x*tmap_tw +tmap_x, y*tmap_th+tmap_y,tmap_tw,tmap_th)
       }else if (tmap[y][x]==2) {
-        ctx.drawImage(img_gazon_route, x*tmpa_tw +tmap_x, y*tmpa_th+tmap_y,tmpa_tw,tmpa_th)
+        ctx.drawImage(img_gazon_route, x*tmap_tw +tmap_x, y*tmap_th+tmap_y,tmap_tw,tmap_th)
       }
     }
   }
