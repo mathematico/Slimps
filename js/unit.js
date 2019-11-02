0327488e5b9f5244992de8517516340040ca2035
InsanityForce <53578624+InsanityForce@users.noreply.github.com>
2019-11-02 02:12:08 +0100

    Update unit.js
    Commented off variable declarations that made Edge crash.
    Added a deleteme boolean in constructor that indicates whether  that unit should  still be displayed and updated, or  can be overwritted by a  main.js add_unit(u)
    Created a function  ysort that can be used as an argument with Array.sort(_) to reorder units by y position, for drawing.

--- js/unit.js
+++ js/unit.js
@@ -1,19 +1,26 @@
+//functions for sorting an array of Units,   usage :    A_unites.sort(ysort)
+function ysort(a,b) {
+  return a.y > b.y ? 1 : (a.y == b.y ? 0 : -1)
+  }
+
+
 //Base class for Units, do not use directly. Use classes that inherit from it.
 
 var unitTexture = new Image(32, 32);
-unitTexture.src="./Resources/patate.png"
+unitTexture.src = "./Resources/patate.png" ;
 
 class Unit {
-  texAtlas  = null;
-  x = 0;
-  y = 0;
-  width = 32;
-  heigth = 32;
-  hp = 1;
-  atk = 1;
-  spd = 1 ;
+  //~ texAtlas;
+  //~ x = 0;
+  //~ y = 0;
+  //~ width = 32;
+  //~ heigth = 32;
+  //~ hp = 1;
+  //~ atk = 1;
+  //~ spd = 1 ;
 
   constructor(x,y) {
+    this.deleteme = false // if true, Unit won't be drawn and may be replaced in A_Unites
     this.x = x;
     this.y = y;
     this.hp = 1;
