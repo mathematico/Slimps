930151a76ed8b2d5d1bc7bf36c797e38af979c48
InsanityForce <53578624+InsanityForce@users.noreply.github.com>
2019-11-05 23:19:56 +0100

    I had  uploaded  the wrong one lol

--- js/display.js
+++ js/display.js
@@ -1,26 +1,40 @@
-function opentab(tabname) {
-  var i;
-  var x = document.getElementsByClassName("tab");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  document.getElementById(tabname).style.display = "block";
+//takes care of HTML elements other than the canvas
+
+var buildinginfo_element = document.getElementById("buildinginfomenu");
+var selectbuilding_element = document.getElementById("selectbuildingmenu");
+var is_constructing = false;
+
+function openlefttab(which) {
+	if (which == 0) {
+		buildinginfo_element.style.display = "block";
+		selectbuilding_element.style.display = "none";
+		is_constructing = false;
+	}
+	if (which == 1) {
+		selectbuilding_element.style.display = "block";
+		buildinginfo_element.style.display = "none";
+		is_constructing = true;
+	}
 }
-var close = document.getElementsByClassName("closebtn");
-var i;
 
-// Loop through all close buttons
-for (i = 0; i < close.length; i++) {
-  // When someone clicks on a close button
-  close[i].onclick = function(){
 
-    // Get the parent of <span class="closebtn"> (<div class="alert">)
-    var div = this.parentElement;
+function on_selected_building_type(i) {
+	selected_building_type = i;  //in main.js
+	//~ console.log("on_selected_building_type ",selected_building_type);
+	selected_building_preview_element.src = map_building_type_to_img.get(selected_building_type).src;
+}
 
-    // Set the opacity of div to 0 (transparent)
-    div.style.opacity = "0";
 
-    // Hide the div after 600ms (the same amount of milliseconds it takes to fade out)
-    setTimeout(function(){ div.style.display = "none"; }, 600);
-  }
+function display_info_on_building_at(x,y) {
+	let building = tmap_buildings[x][y];  // without "let" this would overwrite the building constructor !!!!!
+	buildinginfo_element.innerHTML = "Building is of type "+building.type;
+}
+
+
+function display_ressources_element() {
+	var temprestext = "";
+	for (var i = 0; i <= ressources_stock.length-1;++i ) {
+		temprestext = temprestext + "<br>" + ressources[i]+ " : "+  ressources_stock[i] ;
+	}
+	ressources_list_element.innerHTML =temprestext;
 }
